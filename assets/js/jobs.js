3af10fbefe3ea7bbd6f33c7855645351cc0a5f6c
am <asd>
2024-06-05 23:50:00 +0100

    personalizacion template #2

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -1,7 +1,7 @@
 function calculateWorkDurations() {
     const jobs = [
-        { company: "Auren", startDate: new Date(2020, 4, 1), endDate: new Date(2023, 2, 1) }, // May 2020 to March 2023
-        { company: "Calyx", startDate: new Date(2023, 2, 1), endDate: new Date() } // March 2023 to present
+        { company: "Auren Argentina", startDate: new Date(2022, 8, 1), endDate: new Date(2023, 2, 1) }, // August 2022 to February 2023
+        { company: "Calyx Servicios S.A", startDate: new Date(2023, 2, 1), endDate: new Date() } // March 2023 to present
     ];
 
     jobs.forEach(job => {
@@ -13,16 +13,35 @@ function calculateWorkDurations() {
             months += 12;
         }
 
-        const duration = `${years} years and ${months} months`;
-
-        // Find element using XPath
-        const xpath = `//*[contains(text(), '${job.company}')]`;
-        const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        const element = result.singleNodeValue;
-
-        if (element) {
-            element.innerText = `${job.company}: ${duration}`;
+        let duration;
+        if (years > 0) {
+            duration = `${years} years and ${months+1} months`;
+        } else {
+            duration = `${months+1} months`;
         }
+
+        const xpath = `//p[contains(text(), '${job.company}')]/following-sibling::span[@class='content-date']`;
+        const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        const dateElement = result.singleNodeValue;
+
+        if (dateElement) {
+            const durationText = document.createElement("span");
+            durationText.style.display = "block";
+            durationText.style.textAlign = "left"; 
+        
+            const image = document.createElement("img");
+            image.src = "/assets/img/dev.png";
+            image.alt = "Duration icon";
+            image.style.marginRight = "5px"; 
+            durationText.appendChild(image);
+        
+            const textNode = document.createTextNode(` ${duration}`);
+            durationText.appendChild(textNode);
+        
+            dateElement.appendChild(document.createElement("br"));
+            dateElement.appendChild(durationText);
+        }
+        
     });
 }
 
